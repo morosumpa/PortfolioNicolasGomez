d662f9f76483450525fa614c96489768d69eb4d4
Nicolas Gomez <nicogomezgarcia1@gmail.com>
2023-11-10 14:25:47 +0100

    commit no se porq peto el grid hay q arreglarlo

--- php/vista.php
+++ php/vista.php
@@ -12,11 +12,11 @@
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   <script defer src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
     integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-  </script>
+    </script>
 
   <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
     integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-  </script>
+    </script>
 
   <link rel="stylesheet" href="../assets/css/bootstrap.css">
   <link rel="stylesheet" href="../assets/css/style.css">
@@ -26,123 +26,130 @@
 <body>
   <header>
 
-  <nav class="navbar navbar-expand-lg  bg-header p-3">
-    <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg  bg-header p-3">
+      <div class="container-fluid">
         <img class="imglogo" src="../img/logoportfolio-transformed.png">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    
-      <div class=" collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav ms-auto ">
-          <li class="nav-item">
-            <a class="nav-link mx-2 active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx-2" href="#">Products</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx-2" href="#">Pricing</a>
-          </li>
-        </ul>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class=" collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ms-auto ">
+            <li class="nav-item">
+              <a class="nav-link mx-2 active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-2" href="#">Products</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-2" href="#">Pricing</a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
     </nav>
-</header>
-  <div class="contenedor">   
+  </header>
   <main>
-  <section class="cajapar">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos minus? Ipsum a possimus dolorum. Culpa ipsam quisquam vitae, cumque dicta, sunt impedit molestias corporis repudiandae, quos nihil. Delectus, sed?</p>
-    </section>
-    <section class="cajaimagen">
-    <img src="../img/logoportfolio-transformed.png">
-   </section>
+    <div class="contenedor">
 
-    <section class="cajaimagen">
-    <img src="../img/logoportfolio-transformed.png">
-    </section>
-    <section class="cajaimpar">
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos minus? Ipsum a possimus dolorum. Culpa ipsam quisquam vitae, cumque dicta, sunt impedit molestias corporis repudiandae, quos nihil. Delectus, sed?</p>
-    </section>
-
-    <section class="cajapar">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos minus? Ipsum a possimus dolorum. Culpa ipsam quisquam vitae, cumque dicta, sunt impedit molestias corporis repudiandae, quos nihil. Delectus, sed?</p>
-    </section>
-    <section class="cajaimagen">
-    <img src="../img/logoportfolio-transformed.png">
-  </section>
-
-    <section class="cajaimagen">
-    <img src="../img/logoportfolio-transformed.png">
-  </section>
-    <section class="cajaimpar">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos minus? Ipsum a possimus dolorum. Culpa ipsam quisquam vitae, cumque dicta, sunt impedit molestias corporis repudiandae, quos nihil. Delectus, sed?</p>
-    </section>
-
-    
-    <section class="cajapar">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos minus? Ipsum a possimus dolorum. Culpa ipsam quisquam vitae, cumque dicta, sunt impedit molestias corporis repudiandae, quos nihil. Delectus, sed?</p>
-    </section>
-<section class="cajaimagen">
-<img src="../img/logoportfolio-transformed.png">
-</section>
-    
-    <section class="cajaimagen">
-    <img src="../img/logoportfolio-transformed.png">
-   </section>
-    <section class="cajaimpar">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos minus? Ipsum a possimus dolorum. Culpa ipsam quisquam vitae, cumque dicta, sunt impedit molestias corporis repudiandae, quos nihil. Delectus, sed?</p>
-    </section>
-
-    <section class="cajadatos">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos minus? Ipsum a possimus dolorum. Culpa ipsam quisquam vitae, cumque dicta, sunt impedit molestias corporis repudiandae, quos nihil. Delectus, sed?</p>
-    </section>
-    <section class="cajaimagen">
-    <img src="../img/logoportfolio-transformed.png">
-   </section>
-
-  </main>
-</div>
-<footer class="bg-footer text-center text-lg-start ">
-  <!-- Grid container -->
-  <div class="container p-4">
-    <!--Grid row-->
-    <div class="row">
-      <!--Grid column-->
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Footer text</h5>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
-          aliquam voluptatem veniam, est atque cumque eum delectus sint!
+      <section class="cajapar">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos minus? Ipsum a possimus dolorum. Culpa
+          ipsam quisquam vitae, cumque dicta, sunt impedit molestias corporis repudiandae, quos nihil. Delectus, sed?
         </p>
-      </div>
-      <!--Grid column-->
+      </section>
+      <section class="cajaimagen">
+        <img src="../img/logoportfolio-transformed.png">
+      </section>
 
-      <!--Grid column-->
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Footer text</h5>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
-          aliquam voluptatem veniam, est atque cumque eum delectus sint!
+      <section class="cajaimagen">
+        <img src="../img/logoportfolio-transformed.png">
+      </section>
+      <section class="cajaimpar">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos minus? Ipsum a possimus dolorum. Culpa
+          ipsam quisquam vitae, cumque dicta, sunt impedit molestias corporis repudiandae, quos nihil. Delectus, sed?
         </p>
-      </div>
-      <!--Grid column-->
+      </section>
+
+      <section class="cajapar">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos minus? Ipsum a possimus dolorum. Culpa
+          ipsam quisquam vitae, cumque dicta, sunt impedit molestias corporis repudiandae, quos nihil. Delectus, sed?
+        </p>
+      </section>
+      <section class="cajaimagen">
+        <img src="../img/logoportfolio-transformed.png">
+      </section>
+
+      <section class="cajaimagen">
+        <img src="../img/logoportfolio-transformed.png">
+      </section>
+      <section class="cajaimpar">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos minus? Ipsum a possimus dolorum. Culpa
+          ipsam quisquam vitae, cumque dicta, sunt impedit molestias corporis repudiandae, quos nihil. Delectus, sed?
+        </p>
+      </section>
+
+
+      <section class="cajapar">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos minus? Ipsum a possimus dolorum. Culpa
+          ipsam quisquam vitae, cumque dicta, sunt impedit molestias corporis repudiandae, quos nihil. Delectus, sed?
+        </p>
+      </section>
+      <section class="cajaimagen">
+        <img src="../img/logoportfolio-transformed.png">
+      </section>
+
+      <section class="cajaimagen">
+        <img src="../img/logoportfolio-transformed.png">
+      </section>
+      <section class="cajadatos">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos minus? Ipsum a possimus dolorum. Culpa
+          ipsam quisquam vitae, cumque dicta, sunt impedit molestias corporis repudiandae, quos nihil. Delectus, sed?
+        </p>
+      </section>
+    
     </div>
-    <!--Grid row-->
-  </div>
-  <!-- Grid container -->
+  </main>
+  <footer class="bg-footer text-center text-lg-start ">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Footer text</h5>
 
-  <!-- Copyright -->
-  <div class="text-center p-3 bg-copyright">
-    © 2020 Copyright:
-    <a class="text-dark" href="https://mdbootstrap.com/">Nicolás Gómez García</a>
-  </div>
-  <!-- Copyright -->
-</footer>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+            molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
+            aliquam voluptatem veniam, est atque cumque eum delectus sint!
+          </p>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Footer text</h5>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+            molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
+            aliquam voluptatem veniam, est atque cumque eum delectus sint!
+          </p>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3 bg-copyright">
+      © 2020 Copyright:
+      <a class="text-dark" href="https://mdbootstrap.com/">Nicolás Gómez García</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
 
 </body>
 
